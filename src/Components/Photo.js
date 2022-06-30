1571819d989da816cadca530c30932fe752a8ba8
ksat8384 <ksat8384@gmail.com>
2022-06-30 17:55:44 +0530

    Added prop types

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Photo(props) {
   const { post, onRemovePhoto } = props;
@@ -22,5 +23,10 @@ function Photo(props) {
     </figure>
   );
 }
+
+Photo.propTypes = {
+  post: PropTypes.object.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default Photo;
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Photo from "./Photo";
+import PropTypes from "prop-types";
 
 function Photowall(props) {
   const { posts, onRemovePhoto } = props;
   return (
     <div className="photoGrid">
       {posts.map((post, index) => (
-          <Photo post={post} key={index} onRemovePhoto={onRemovePhoto} />
+        <Photo post={post} key={index} onRemovePhoto={onRemovePhoto} />
       ))}
     </div>
   );
 }
+
+Photowall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default Photowall;
